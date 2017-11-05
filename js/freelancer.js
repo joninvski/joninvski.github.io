c410786b6b8d7b8e199acc6db0f44bc4dc32e986
Joao Trindade <trindade.joao@gmail.com>
2017-11-05 18:51:30 +0000

    Correct blog submenu

--- js/freelancer.js
+++ js/freelancer.js
@@ -28,7 +28,7 @@ $(function() {
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+    target: '.navbar-top'
 })
 
 // Closes the Responsive Menu on Menu Item Click
